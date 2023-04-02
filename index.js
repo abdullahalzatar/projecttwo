504a26e879f5e949470a7a4f38c6ed4f0e8b74dd
Abdullah al-zatar <abdulah.al-zatar@pwc.com>
2023-04-02 11:18:45 +0300

    add middleware file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const errorHandler = require("./middleware/app.js");
+const logger = require("./middleware/logger");
 const database = require("./database");
 const router = require("./router");
 
@@ -7,6 +9,8 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
+app.use(logger);
+app.use(errorHandler);
 app.use("/api", router);
 
 
