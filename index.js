f113978b94799b76152630ad40ca306cff48bbc6
Abdullah al-zatar <abdulah.al-zatar@pwc.com>
2023-04-03 11:16:05 +0300

    add mongodb

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const errorHandler = require("./middleware/app.js");
 const logger = require("./middleware/logger");
 const server = require("./server");
 const router = require("./router");
+const cookieParser = require("cookie-parser");
+const connectDB = require('./config/db');
 
 const app = express();
+app.use(cookieParser());
+connectDB();
 
 const port = process.env.port || 3000;
 
